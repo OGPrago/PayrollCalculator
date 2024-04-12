b05b3d63fb7b57094510dbaab65562118ddb7de6
Jeremy <ogprago@gmail.com>
2024-04-11 21:06:30 -0400

    Updated Payroll Calculator with overtime pay

--- src/main/java/com/jg/PayrollCalc.java
+++ src/main/java/com/jg/PayrollCalc.java
@@ -20,8 +20,14 @@ public class PayrollCalc {
         System.out.println("Enter pay rate ");
         payRate = scanner.nextFloat();
 
-        //Calculating user's gross pay
-        grossPay = hoursWorked * payRate;
+        //Calculating user's gross pay, and if they worked overtime
+        if(hoursWorked > 40) {
+            float overtimeRate = 1.5f;
+            grossPay = (hoursWorked * overtimeRate) * payRate;
+        }
+        else {
+            grossPay = hoursWorked * payRate;
+        }
         //Display employee's name and gross pay
         System.out.println(name + " gross pay is " + grossPay);
     }
